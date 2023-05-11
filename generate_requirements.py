de56811726c1ed8eda2b9365d2d9e4e314736359
Nitsh-sha <nitsha.shep@gmail.com>
2023-05-11 14:22:36 -0700

    Revert "requirements.txt added"
    This reverts commit c7e2cbd455bc2ae2f58d7b3519b9f736373dd40b.

--- generate_requirements.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-# Generate the requirements.txt file
-subprocess.run(["pipenv", "lock", "-r", "--dev"], capture_output=True, text=True)
-
-# Move the generated Pipfile.lock to requirements.txt
-subprocess.run(["mv", "Pipfile.lock", "requirements.txt"])
